c62b1d7dc53af9542f67a1635917a9e77ef611c3
Paula-HC <paherazoc@unal.edu.co>
2025-06-09 22:10:51 -0500

    Lab 5

--- homework/homework.py
+++ homework/homework.py
@@ -7,20 +7,20 @@ Escriba el codigo que ejecute la accion solicitada.
 
 def clean_campaign_data():
     """
-    En esta tarea se le pide que limpie los datos de una campaña de
+    En esta tarea se le pide que limpie los datos de una camp de
     marketing realizada por un banco, la cual tiene como fin la
-    recolección de datos de clientes para ofrecerls un préstamo.
+    recolección de datos de clients para ofrecerls un préstamo.
 
     La información recolectada se encuentra en la carpeta
-    files/input/ en varios archivos csv.zip comprimidos para ahorrar
+    files/input/ en varios files csv.zip comprimidos para ahorrar
     espacio en disco.
 
-    Usted debe procesar directamente los archivos comprimidos (sin
-    descomprimirlos). Se desea partir la data en tres archivos csv
+    Usted debe procesar directamente los files comprimidos (sin
+    descomprimirlos). Se desea partir la data en tres files csv
     (sin comprimir): client.csv, campaign.csv y economics.csv.
-    Cada archivo debe tener las columnas indicadas.
+    Cada file debe tener las columnas indicadas.
 
-    Los tres archivos generados se almacenarán en la carpeta files/output/.
+    Los tres files generados se almacenarán en la carpeta files/output/.
 
     client.csv:
     - client_id
@@ -49,6 +49,82 @@ def clean_campaign_data():
 
 
     """
+
+    import os
+    import zipfile
+    import pandas as pd
+    from glob import glob
+
+    dir_in = os.path.join("files", "input")
+    
+    dir_out = os.path.join("files", "output")
+    
+    os.makedirs(dir_out, exist_ok=True)
+
+    data = []
+
+    files_zip = glob(os.path.join(dir_in, "*.zip"))
+    for file_zip in files_zip:
+    
+        with zipfile.ZipFile(file_zip, 'r') as zip_abierto:
+    
+            for file in zip_abierto.namelist():
+                if file.endswith(".csv"):
+    
+                    with zip_abierto.open(file) as contenido:
+                        df = pd.read_csv(contenido)
+                        data.append(df)
+
+    if not data:
+        return
+
+    datos = pd.concat(data, ignore_index=True)
+
+    # Datos del cliente
+    clients = pd.DataFrame({
+        "client_id": datos["client_id"],
+        "age": datos["age"],
+        "job": datos["job"].str.replace(".", "", regex=False).str.replace("-", "_", regex=False),
+        "marital": datos["marital"],
+        "education": datos["education"].str.replace(".", "_", regex=False)
+    })
+    
+    clients["education"].replace("unknown", pd.NA, inplace=True)
+    
+    clients["credit_default"] = datos["credit_default"].str.lower().eq("yes").astype(int)
+    
+    clients["mortgage"] = datos["mortgage"].str.lower().eq("yes").astype(int)
+    
+    clients.to_csv(os.path.join(dir_out, "client.csv"), index=False)
+
+    # Datos de la camp
+    camp = pd.DataFrame({
+        "client_id": datos["client_id"],
+        "number_contacts": datos["number_contacts"],
+        "contact_duration": datos["contact_duration"],
+        "previous_campaign_contacts": datos["previous_campaign_contacts"],
+        "previous_outcome": datos["previous_outcome"].str.lower().eq("success").astype(int),
+        "campaign_outcome": datos["campaign_outcome"].str.lower().eq("yes").astype(int),
+    })
+
+    meses = {
+        "jan": "01", "feb": "02", "mar": "03", "apr": "04", "may": "05", "jun": "06",
+        "jul": "07", "aug": "08", "sep": "09", "oct": "10", "nov": "11", "dec": "12"
+    }
+    
+    dias = datos["day"].astype(str).str.zfill(2)
+    
+    mes_numerico = datos["month"].str.lower().map(meses)
+    
+    camp["last_contact_date"] = "2022-" + mes_numerico + "-" + dias
+    
+    camp.to_csv(os.path.join(dir_out, "campaign.csv"), index=False)
+
+    # Información económica
+    
+    eco = datos[["client_id", "cons_price_idx", "euribor_three_months"]]
+    
+    eco.to_csv(os.path.join(dir_out, "economics.csv"), index=False)
 
     return
 
